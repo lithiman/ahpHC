5194d4de0d59f01c4958a86d08c867e2bcb86620
Anthony <lithiman@gmail.com>
2016-07-20 18:02:24 -0500

    cadena de conexion

--- webAHPHC/webAHPHC/Models/IdentityModels.cs
+++ webAHPHC/webAHPHC/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace webAHPHC.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("FBADB", throwIfV1Schema: false)
         {
         }
 
